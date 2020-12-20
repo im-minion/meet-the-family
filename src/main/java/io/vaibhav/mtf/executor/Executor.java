79ee00fb75a4b2de38abc813639f1ec8bba7a2dd
im-minion <vaibhavminiyar@gmail.com>
2020-12-20 19:57:34 +0530

    tested all the scenario. Refactored!

--- src/main/java/io/vaibhav/mtf/executor/Executor.java
+++ src/main/java/io/vaibhav/mtf/executor/Executor.java
@@ -36,51 +36,49 @@ public class Executor implements IExecutor {
             StringBuilder result = new StringBuilder();
             if (null == this.currentFamilyMembers || null == this.currentFamilyMembers.get(name)) {
                 result.append(Constants.PERSON_NOT_FOUND);
-                System.out.println(result.toString().trim());
-                return;
-//                Ideally should throw exception but as per the requirement just log
-//                throw new ExecutorException(String.format("No Family Member Exists with name %s.", name), new RuntimeException());
-            }
-
-            switch (relation) {
-                case Constants.SON:
-                    getChild(result, name, Gender.MALE);
-                    break;
-                case Constants.DAUGHTER:
-                    getChild(result, name, Gender.FEMALE);
-                    break;
-                case Constants.SIBLINGS:
-                    getSiblings(result, name);
-                    break;
-                case Constants.PATERNAL_UNCLE: //Father’s brothers
-                    Node fatherNode = this.currentFamilyMembers.get(name).getFather();
-                    getSiblingsWithGender(fatherNode, result, name, Gender.MALE);
-                    break;
-                case Constants.PATERNAL_AUNT: //Father’s sisters
-                    Node fatherNode2 = this.currentFamilyMembers.get(name).getFather();
-                    getSiblingsWithGender(fatherNode2, result, name, Gender.FEMALE);
-                    break;
-                case Constants.MATERNAL_UNCLE: //Mother’s brothers
-                    Node motherNode = this.currentFamilyMembers.get(name).getMother();
-                    getSiblingsWithGender(motherNode, result, name, Gender.MALE);
-                    break;
-                case Constants.MATERNAL_AUNT: //Mother’s sisters
-                    Node motherNode2 = this.currentFamilyMembers.get(name).getMother();
-                    getSiblingsWithGender(motherNode2, result, name, Gender.FEMALE);
-                    break;
-                case Constants.SISTER_IN_LAW: //Spouse’s sisters,  Wives of siblings
-                    Node spouseNode = this.currentFamilyMembers.get(name).getSpouse();
-                    getSiblingsWithGender(spouseNode, result, name, Gender.FEMALE);
-                    getSiblingsWives(result, name);
-                    break;
-                case Constants.BROTHER_IN_LAW: //Spouse’s brothers,  Husbands of siblings
-                    Node spouseNode2 = this.currentFamilyMembers.get(name).getSpouse();
-                    getSiblingsWithGender(spouseNode2, result, name, Gender.MALE);
-                    getSiblingsHusbands(result, name);
-                    break;
-                default:
-                    System.out.println("INVALID INPUT");
-                    break;
+                // Ideally should throw exception but as per the requirement just logging it
+                // throw new ExecutorException(String.format("No Family Member Exists with name %s.", name), new RuntimeException());
+            } else {
+                switch (relation) {
+                    case Constants.SON:
+                        getChild(result, name, Gender.MALE);
+                        break;
+                    case Constants.DAUGHTER:
+                        getChild(result, name, Gender.FEMALE);
+                        break;
+                    case Constants.SIBLINGS:
+                        getSiblings(result, name);
+                        break;
+                    case Constants.PATERNAL_UNCLE: //Father’s brothers
+                        Node fatherNode = this.currentFamilyMembers.get(name).getFather();
+                        getSiblingsWithGender(fatherNode, result, name, Gender.MALE);
+                        break;
+                    case Constants.PATERNAL_AUNT: //Father’s sisters
+                        Node fatherNode2 = this.currentFamilyMembers.get(name).getFather();
+                        getSiblingsWithGender(fatherNode2, result, name, Gender.FEMALE);
+                        break;
+                    case Constants.MATERNAL_UNCLE: //Mother’s brothers
+                        Node motherNode = this.currentFamilyMembers.get(name).getMother();
+                        getSiblingsWithGender(motherNode, result, name, Gender.MALE);
+                        break;
+                    case Constants.MATERNAL_AUNT: //Mother’s sisters
+                        Node motherNode2 = this.currentFamilyMembers.get(name).getMother();
+                        getSiblingsWithGender(motherNode2, result, name, Gender.FEMALE);
+                        break;
+                    case Constants.SISTER_IN_LAW: //Spouse’s sisters,  Wives of siblings
+                        Node spouseNode = this.currentFamilyMembers.get(name).getSpouse();
+                        getSiblingsWithGender(spouseNode, result, name, Gender.FEMALE);
+                        getSiblingsWives(result, name);
+                        break;
+                    case Constants.BROTHER_IN_LAW: //Spouse’s brothers,  Husbands of siblings
+                        Node spouseNode2 = this.currentFamilyMembers.get(name).getSpouse();
+                        getSiblingsWithGender(spouseNode2, result, name, Gender.MALE);
+                        getSiblingsHusbands(result, name);
+                        break;
+                    default:
+                        System.out.println("INVALID INPUT");
+                        break;
+                }
             }
             if (result.length() > 0) {
                 System.out.println(result.toString().trim());
